b3b4ed97d3d40522a8a053ba167c50cb3e13d01f
diegodov <diegodd07@gmail.com>
2024-01-02 17:37:36 -0300

    Finalizacion de codigo

--- stringFilter/Program.cs
+++ stringFilter/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿/* 
+ * * Autor: @diegodov
+ * * 
+ * * Descripción: Uso de do-while para validar una cadena de texto 
+ * *
+ * */
+
+bool valido = false;
+
+do
+{ 
+    Console.Write("Ingrese el rol: ");
+
+    string? entrada = Console.ReadLine();
+
+    string? orden = entrada.Trim().ToLower();
+
+    if (orden == "administrador" || orden == "manager" || orden == "user" )
+    {
+        Console.WriteLine($"El rol ({entrada}) es valido");
+        valido = true;
+    } else
+    {
+        Console.WriteLine("El rol ingresado no es valido");
+    }
+
+} while (valido == false);
+
+
+
+
+
+
