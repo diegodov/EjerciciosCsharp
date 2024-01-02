287bc9e876e31f58cfaced60fdbbb2593693d5b2
diegodov <diegodd07@gmail.com>
2024-01-02 16:44:00 -0300

    Primer commit del primer ejercicio de consola en C#

---
+++ intFilter/Program.cs
@@ -0,0 +1,25 @@
+﻿// capture user input in a string variable named readResult
+
+int numericValue = 0;
+
+bool validNumber = false;
+
+do
+{
+    Console.Write("Ingrese un entero del 5 al 10: ");
+
+    string readResult = Console.ReadLine();
+
+    validNumber = int.TryParse(readResult, out numericValue);
+
+    if (numericValue < 5 || numericValue > 10)
+    {
+        Console.WriteLine("El valor ingresado no esta en el rango entre 5 y 10.");
+        validNumber = false;
+    }
+    else
+    {
+        Console.WriteLine($"Su numero ({numericValue}) ha sido aceptado.");
+    }
+
+} while (validNumber == false);
